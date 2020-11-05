ef247701d09272625f2175d48f57fce72482a16e
miyamae <yu1ro.mya@gmail.com>
2020-11-05 13:03:28 +0900

    update: AwsCdkFargatePlaygroundStack create FargateService

--- lib/aws-cdk-fargate-playground-stack.ts
+++ lib/aws-cdk-fargate-playground-stack.ts
@@ -1,9 +1,31 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecsPatterns from "@aws-cdk/aws-ecs-patterns";
 
 export class AwsCdkFargatePlaygroundStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "Vpc");
+    const cluster = new ecs.Cluster(this, "Cluster", {
+      vpc,
+    });
+    const loadBalancedFargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(
+      this,
+      "Service",
+      {
+        cluster,
+        memoryLimitMiB: 1024,
+        cpu: 512,
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+        },
+      }
+    );
+
+    loadBalancedFargateService.targetGroup.configureHealthCheck({
+      path: "/custom-health-path",
+    });
   }
 }
